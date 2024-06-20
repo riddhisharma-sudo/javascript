acbe710ac0b6e4be9a59c831d171103e83c9921d
Riddhi Sharma <119350954+riddhisharma-sudo@users.noreply.github.com>
2024-06-21 01:19:16 +0530

    Add files via upload

---
+++ javascript/1.js
@@ -0,0 +1,14 @@
+console.log("hello js");
+console.log("how are you");
+name = "tony stark";
+console.log(name);
+age= 24;
+price = 99.99;
+
+console.log(price);
+x = null;
+console.log(x);
+y = undefined;
+console.log(y);
+isfollow= false;
+console.log(isfollow);
